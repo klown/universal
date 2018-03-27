83728020652de4d57578d39f606f1b02e4d35d59
Joseph Scheuhammer <clown@alum.mit.edu>
2018-03-27 13:56:23 -0400

    NO-JIRA:  Improving test coverage for process reporter modules.
    Modified "all-tests" in order to make processes bridge tests node-based
    as opposed to web based.

--- tests/all-tests.js
+++ tests/all-tests.js
@@ -89,6 +89,7 @@ var testIncludes = [
     "../gpii/node_modules/contextManager/test/ContextManagerTests.js",
     "../gpii/node_modules/singleInstance/test/SingleInstanceTests.js",
     "../gpii/node_modules/eventLog/test/EventLogTests.js",
+    "../gpii/node_modules/processReporter/test/ProcessesBridgeTests.js",
     "../gpii/node_modules/userListeners/test/all-tests.js"
 ];
 
